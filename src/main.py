d544223728d41971ea02c3b2803126e823827e1b
Talha Razzaq <115282349+Talha-2@users.noreply.github.com>
2025-07-23 16:01:36 +0500

    src 2nd commit

---
+++ src/main.py
@@ -0,0 +1,40 @@
+from typing import Dict 
+import csv
+import json
+from src.StateGraph.nodes import construct_workflow
+from src.StateGraph.ticket_state import TicketState
+
+graph = construct_workflow()
+
+def run_agent(ticket: Dict[str, str]):
+    initial_state = TicketState(
+        ticket=ticket,
+        category="",
+        context=[],
+        draft="",
+        review_feedback="",
+        retry_count=0,
+        final_response=None
+    )
+    result = graph.invoke(initial_state, config={"thread_id": "ticket-001", "recursion_limit": 50})
+    return result
+
+
+if __name__ == "__main__":
+   
+    with open("escalation_log.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Subject", "Description", "Category", "Draft", "Feedback"])
+
+    test_ticket = {
+    "subject": "Unauthorized login attempt",
+    "description": "I received an alert about a login attempt from an unknown device in another country."
+}
+
+
+    result = run_agent(test_ticket)
+    print("Final State:", json.dumps(result, indent=2))
+    if result["final_response"]:
+        print("Final Response:", result["final_response"])
+    else:
+        print("Escalated to human review. Check escalation_log.csv")
